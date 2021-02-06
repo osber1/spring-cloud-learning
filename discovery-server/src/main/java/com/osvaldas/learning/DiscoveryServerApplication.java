dca78f386e14633c013dbf0e0b657bd88e381a4c
Osvaldas Bernatavičius <osvaldas.bernatavicius@gmail.com>
2021-02-06 17:04:17 +0200

    Eureka added.

--- discovery-server/src/main/java/com/osvaldas/learning/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/osvaldas/learning/DiscoveryServerApplication.java
@@ -2,12 +2,12 @@ package com.osvaldas.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderServiceApplication {
+@EnableEurekaServer
+public class DiscoveryServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class, args);
+        SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 }
