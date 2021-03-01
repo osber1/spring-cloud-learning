0537f27be484e4fe70a92cd024c0811fe351b78f
Osvaldas Bernatavičius <osvaldas.bernatavicius@gmail.com>
2021-03-01 20:04:20 +0200

    Inventory service added.

---
+++ inventory-service/src/main/java/com/osvaldas/learning/controller/InventoryController.java
@@ -0,0 +1,24 @@
+package com.osvaldas.learning.controller;
+
+import com.osvaldas.learning.model.Inventory;
+import com.osvaldas.learning.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/inventory")
+public class InventoryController {
+
+    private final InventoryRepository repository;
+
+    @GetMapping("{skuCode}")
+    Boolean inStock(@PathVariable String skuCode) {
+        Inventory inventory = repository.findBySkuCode(skuCode)
+                .orElseThrow(() -> new RuntimeException("Cannot find product by sku code: " + skuCode));
+        return inventory.getStock() > 0;
+    }
+}
