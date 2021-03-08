155f7791f38c5194cfda7bbb362c76dc75d8c9db
Osvaldas <60891426+osber1@users.noreply.github.com>
2021-03-08 09:58:11 +0200

    Update NotificationServiceApplication.java

--- notification-service/src/main/java/com/osvaldas/learning/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/osvaldas/learning/NotificationServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.function.Consumer;
 
-@EnableEurekaClient
+// @EnableEurekaClient
 @SpringBootApplication
 public class NotificationServiceApplication {
     public static void main(String[] args) {
